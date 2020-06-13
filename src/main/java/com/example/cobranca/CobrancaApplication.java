1bd4c2ce7592ae8c43dae8da71b5d2d7efd1b8c7
Guilherme Tessaro <guilherme.tessaro27@gmail.com>
2020-06-13 11:13:13 -0300

    ajuste url para pegar o context path

--- src/main/java/com/example/cobranca/CobrancaApplication.java
+++ src/main/java/com/example/cobranca/CobrancaApplication.java
@@ -5,6 +5,9 @@ import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -18,5 +21,15 @@ public class CobrancaApplication {
 	@Bean
 	public FixedLocaleResolver localeResolver() {
 		return new FixedLocaleResolver(new Locale("pt","BR"));
+	}
+	
+	@Configuration
+	public static class MvcConfig extends WebMvcConfigurerAdapter{
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulo");
+		}
+		
 	}
 }
